ee80e11237fdd4fd4adc8a276ad0126c0b146b54
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-04-03 11:42:14 +0200

    create bug form gets handled

--- src/com/core/buga/CreateBugActivity.java
+++ src/com/core/buga/CreateBugActivity.java
@@ -2,14 +2,31 @@ package com.core.buga;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.telephony.gsm.SmsMessage.SubmitPdu;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class CreateBugActivity extends Activity {
-
+public class CreateBugActivity extends Activity implements android.view.View.OnClickListener  {
+	
+	private EditText bugTitle;
+	private EditText bugDescription;
+	private EditText projectTitle;
+	private Button	 submitBtn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_bug);
+		
+		bugTitle = (EditText) findViewById(R.id.edit_text_title);
+		bugDescription = (EditText) findViewById(R.id.edittext_bugdescription);
+		projectTitle = (EditText) findViewById(R.id.edit_text_project_title);
+		submitBtn = (Button) findViewById(R.id.button_bugsubmit);
+		
+		submitBtn.setOnClickListener(this);
 	}
 
 	@Override
@@ -19,4 +36,18 @@ public class CreateBugActivity extends Activity {
 		return true;
 	}
 
+	@Override
+	public void onClick(View v) {
+		if(v.equals(submitBtn)){
+			if(bugTitle.getText().toString().length() > 0 &&
+				bugDescription.getText().toString().length() > 0 &&
+				projectTitle.getText().toString().length() > 0
+				){
+				//save content
+			}else{
+				Toast.makeText(this, R.string.bugform_fail, Toast.LENGTH_LONG).show();
+			}
+		}
+		
+	}
 }
