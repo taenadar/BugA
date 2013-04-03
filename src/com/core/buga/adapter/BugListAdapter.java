5680e0383441607681443fbe6b980319ccaec101
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-04-03 16:28:48 +0200

    Adapter update

--- src/com/core/buga/adapter/BugListAdapter.java
+++ src/com/core/buga/adapter/BugListAdapter.java
@@ -1,32 +1,54 @@
 package com.core.buga.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import com.core.buga.models.*;
+
 public class BugListAdapter extends BaseAdapter {
 
+	private List<Bug> bugitems = new ArrayList<Bug>(); 
+	private Context context;
+	
+	public BugListAdapter(final Context context) {
+		this.context = context;
+	}
+	
+	public void setList(final List<Bug> bugItems) {
+		this.bugitems = bugItems;
+		notifyDataSetChanged();
+	}
+	
 	@Override
 	public int getCount() {
-		// TODO Auto-generated method stub
+		if(bugitems != null){
+			return bugitems.size();
+		}
 		return 0;
 	}
 
 	@Override
-	public Object getItem(int arg0) {
-		// TODO Auto-generated method stub
+	public Object getItem(int position) {
+		if(bugitems != null){
+			return bugitems.get(position);
+		}
 		return null;
 	}
 
 	@Override
-	public long getItemId(int arg0) {
-		// TODO Auto-generated method stub
-		return 0;
+	public long getItemId(int position) {
+		return position;
 	}
 
 	@Override
-	public View getView(int arg0, View arg1, ViewGroup arg2) {
-		// TODO Auto-generated method stub
+	public View getView(int postion, View convertView, ViewGroup arg2) {
+		
+		
 		return null;
 	}
 
